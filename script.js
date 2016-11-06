2e2d7bae4ed27c045625142b58a1bad44d0bfd8a
renukasingh04 <renuka.singh414@gmail.com>
2016-11-06 23:25:57 +0530

    Update script.js
    Changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var myApp = angular.module('myApp',[]);
 
 myApp.controller('DoubleController', ['$scope', function($scope) {
+  // global declaration
   $scope.data = {};
   $scope.data.location = "Mumbai"
   var unit_Solar_System_Cost = 75000, downpayment = 0; // Rs. 75,000 per kW
@@ -10,52 +11,68 @@ myApp.controller('DoubleController', ['$scope', function($scope) {
     Pune: 9, 
     Bangalore : 5
   };
-  $scope.xe = {};
-  
+  $scope.link = "https://cdn4.iconfinder.com/data/icons/miu/24/circle-info-more-information-detail-outline-stroke-128.png"
+
+  // helper methods
   var getMinvalue = function(val1,val2) {
   	return (val1<val2 ? val1  : val2);
   }
-  
   var getMaxvalue = function(val1,val2) {
   	return (val1>val2 ? val1 :val2);
   }
-  var data = {data1:[],data2:[],data3:[]}
-  
+
 	$scope.$watchCollection('data', function() {
 	  if($scope.data.roofSize && $scope.data.unitConsumed) {
-	   $scope.data.solarSystemSize = getMinvalue($scope.data.unitConsumed/120 , $scope.data.roofSize/100 );
-	   $scope.data.solarUnitProduced = $scope.data.solarSystemSize * 4 * 30;
-	   $scope.data.solarSystemCost  = unit_Solar_System_Cost * $scope.data.solarSystemSize;
+  	   $scope.data.solarSystemSize = getMinvalue($scope.data.unitConsumed/120 , $scope.data.roofSize/100 );
+  	   $scope.data.solarUnitProduced = $scope.data.solarSystemSize * 4 * 30;
+  	   $scope.data.solarSystemCost  = unit_Solar_System_Cost * $scope.data.solarSystemSize;
 	  }
-	  if($scope.data.loanTenure && $scope.data.loanAmount){
-	   $scope.data.EMI = (($scope.data.loanAmount * interest_Rate * $scope.data.loanTenure)/100 + Number($scope.data.loanAmount))/(Number($scope.data.loanTenure)*12);
-	   downpayent = Number($scope.data.solarSystemCost) - Number($scope.data.loanAmount);
-	   $scope.showgraph = Number($scope.data.loanAmount) <= (0.85*$scope.data.solarSystemCost) ? true : false
+	  else {
+	    $scope.data.solarSystemSize = $scope.data.solarUnitProduced = $scope.data.solarSystemCost = undefined;
+	  }
+	  if($scope.data.solarSystemSize && $scope.data.loanTenure && $scope.data.loanAmount){
+  	   $scope.data.EMI = (($scope.data.loanAmount * interest_Rate * $scope.data.loanTenure)/100 + Number($scope.data.loanAmount))/(Number($scope.data.loanTenure)*12);
+  	   downpayent = Number($scope.data.solarSystemCost) - Number($scope.data.loanAmount);
+  	   $scope.showgraph = Number($scope.data.loanAmount) <= (0.85*$scope.data.solarSystemCost) ? true : false
 	  } 
-	  $scope.xe.currentElectrictyBill = $scope.data.unitConsumed * ENUM[$scope.data.location];
-	  $scope.xe.reducedUnits =  getMaxvalue(($scope.data.unitConsumed-$scope.data.solarUnitProduced) , 0);
-	  $scope.xe.reducedElectrictyBill  = $scope.xe.reducedUnits * ENUM[$scope.data.location];
-	  $scope.xe.monthlySavings = $scope.xe.currentElectrictyBill - $scope.xe.reducedElectrictyBill - $scope.data.EMI
-    if($scope.showgraph && $scope.xe.currentElectrictyBill && $scope.data.solarSystemCost && $scope.data.EMI){
-      data.data1=[0], data.data2=[$scope.data.solarSystemCost], data.data3=[downpayent];
-      for(var i = 1;i<=10;i++) {
-        data.data1.push(data.data1[i-1] + $scope.xe.currentElectrictyBill  * 12);
-        data.data2.push(data.data2[i-1] + $scope.xe.reducedElectrictyBill  * 12);
-        if(i <= Number($scope.data.loanTenure)){
-          data.data3.push(data.data3[i-1] + ($scope.xe.reducedElectrictyBill + $scope.data.EMI)*12)
-        }
-        else {
-          data.data3.push(data.data3[i-1] + $scope.xe.reducedElectrictyBill * 12)
-        }
-      }
-      draw();
-    }
+	  else {
+	     $scope.data.EMI = $scope.showgraph = undefined;
+	  }
+    getDataForGraph();
 	})
 	
-  var draw = function(){
+	var getDataForGraph = function(){
+	  // data for garph
+	  var currentElectrictyBill = $scope.data.unitConsumed * ENUM[$scope.data.location];
+	  var reducedUnits =  getMaxvalue(($scope.data.unitConsumed-$scope.data.solarUnitProduced) , 0);
+	  var reducedElectrictyBill  = reducedUnits * ENUM[$scope.data.location];
+	  $scope.monthlySavings = currentElectrictyBill - reducedElectrictyBill - $scope.data.EMI
+    if($scope.showgraph && currentElectrictyBill && $scope.data.solarSystemCost && $scope.data.EMI){
+      var x=[0], data1=[0], data2=[$scope.data.solarSystemCost], data3=[downpayent];
+      for(var i = 1;i<=10;i++) {
+        data1.push(data1[i-1] + (currentElectrictyBill  * 12));
+        data2.push(data2[i-1] + (reducedElectrictyBill  * 12));
+        if(i === Number($scope.data.loanTenure)){
+          x.push("Loan Paid off")
+        }
+        else {
+          x.push(i);
+        }
+        if(i <= Number($scope.data.loanTenure)){
+          data3.push(data3[i-1] + (reducedElectrictyBill + $scope.data.EMI)*12)
+        }
+        else {
+          data3.push(data3[i-1] + reducedElectrictyBill * 12)
+        }
+      }
+      draw(x,data1,data2,data3);
+    }
+	}
+	
+  var draw = function(x,data1,data2,data3){
     Highcharts.chart('container', {
         xAxis: {
-            categories: [0,1,2,3,4,5,6,7,8,9,10],
+            categories: x,
             title: { text: 'Time (years)' }
         },
         title: {
@@ -64,22 +81,23 @@ myApp.controller('DoubleController', ['$scope', function($scope) {
         yAxis: { title: { text: 'Cash outflow on electricity (Rs)' } },
         series: [{
             name: 'No solar,Normal electricity bills',
-            data: data.data1,
+            data: data1,
             color: '#ff0000'
         },
         {     
             name: 'With Solar, no loan',
-            data: data.data2,
+            data: data2,
             color: '#0000ff'
         },
         {
             name: 'With Solar and Oorjan loan',
-            data: data.data3,
+            data: data3,
             color: '#00ff00'
         }]
     });
   }
 }]);
+
 
 
 
